03ca050d09184f5674e4e959a5292b11180e5a74
Code Your Future <onneile99@yahoo.com>
2018-03-15 23:05:29 +0000

    myCommitModuleTest

--- module1.js
+++ module1.js
@@ -8,7 +8,8 @@
 
 /**
   Question 1
-  Comparing a string with ‘Code Your Future’, return true if they are the same or
+  Comparing a string with ‘Code Your Future’,
+   return true if they are the same or
   false otherwise
 
   The function takes the following argument:
@@ -16,34 +17,45 @@
   @returns {boolean} true or false
 */
 
+
 function question1(name1) {
-  if (name1) {
+
+  // let string="Code Your Future";
+  if (name1 === "Code Your Future") {
+   
     // TODO:
-    return;
+    return true;
   }
-}
+  else {
+    return false;
+  }
+};
+
+// console.log(question1());
 
 
 /**
   +++++++++++++++++++++++++++++++++++++++++++
   Question 2:
   IF / ELSE Statements:
-  Check if two numbers are equal, if they are return true or otherwise return false
+  Check if two numbers are equal, if they are return true 
+  or otherwise return false
   @param {number} number1 a regular integer
   @param {number} number2 a regular integer
   @returns {boolean} true or false
 */
 
-// function question2(number1, number2) {
-//   if (number1) {
-//     // TODO:
-//     return;
-//   } else {
-//     // TODO:
-//     return;
-//   }
-// }
-
+function question2(number1, number2) {
+  
+  if (number1 === number2) {
+    // TODO:
+    return true;
+  } else {
+    // TODO:
+    return false;
+  }
+};
+// console.log(question2(199, 204));
 
 /**
   +++++++++++++++++++++++++++++++++++++++++++
@@ -51,93 +63,117 @@ function question1(name1) {
   Is the number even or odd?
   @param {number} number1 a regular integer
   @returns {string} odd or even
-*/
+ */
 
-// function question3(number1) {
-//   if (number1) {
-//     return;
+ function question3 ( number1) {
+  if( number1 % 2 === 0 ) {
+  return 'even';
+  } else  {
+  return 'odd';
+  } 
+};
+  
+
+//  function(number1) {
+//   if( (number1 % 3) === 0 ) {
+//   return true;
+//   } else if (isNaN(number)) {
+//   return 'odd';
+//   } 
+//   else {
+//   return 'even';
 //   }
-// }
-
-
+//   };
+//   console.log(isEven(2019));
 /**
-  +++++++++++++++++++++++++++++++++++++++++++
+  // +++++++++++++++++++++++++++++++++++++++++
+/*
   Question 4:
-  Write a for loop that loops over the numbers from 0 to 100, returning an array containing only every second number. So it should return [ 0, 2, 4, 6, 8, 10 .... etc.]
-
-  @param none
-  @returns [array 0 ... 100] 
-*/
-
+  Write a for loop that loops over the numbers 
+  from 0 to 100, returning an array containing 
+  only every second number. So it should return [ 
+//     0, 2, 4, 6, 8, 10 .... etc.]
+// @param none
+//   @returns [array 0 ... 100] 
+// */
+// // +++++++++++++++++++++++++++++++++++++++++
 // function question4() {
+//   let i=0;
+//   console.log(i)
 //   // TODO
-//   for ( /* ??? */ ) {
+//   for (i=0; i<100; i++ ){
+//     console.log(i);
 //     // TODO
 //   }
-//   return /* ??? */;
+//   return i;
 // }
-
+//  question4();
 
 /**
   +++++++++++++++++++++++++++++++++++++++++++
   Question 5:
-  Write a function called multiply that multiplies two numbers together. It should take two numbers as parameters and return the answer.
-
-  @param { num1, num2 } two mumbers; integer, float, either negative or positive
-  @returns the product of num1 and num2 
-*/
-
-// function question5(num1, num2) {
-//   // TODO
-
-//   return /* ??? */;
-// }
-
-
-/**
+?
+/*
   +++++++++++++++++++++++++++++++++++++++++++
   Question 6:
-  Write a function called multiply that multiplies two numbers together. It should take two numbers as parameters and return the answer.
+  Write a function called multiply 
+  that multiplies two numbers together. 
+  It should take two numbers as parameters and return the answer.
 
-  @param { animArr } an array containing the names of a number of animals
-  @returns { string } in the format...
-    "The array contains { number of animals } animals. These are: {animal1} and {animal2} and {animal3}."
-*/
+// //   @param { animArr } an array containing the 
+// //   names of a number of animals
+// //   @returns { string } in the format...
+// //     "The array contains { number of animals } 
+// //     animals. These are: {animal1} and {animal2} and {animal3}."
+// // */
 
-// function question6( animArr ) {
+// function multiply ( a,b ) {
 //   // TODO
 
-//   return /* ??? */;
+//   return a*b;
 // }
 
 
 /**
   +++++++++++++++++++++++++++++++++++++++++++
   Question 7:
-  Write a function to use a 'person' object containing their details as 5 key/value pairs
+  Write a function to use a 'person' object containing their details
+   as 5 key/value pairs
 
-  @param { personObj } an array containing the names of a number of animals
-  @returns { string } in the format...
-    "Hi, my name is {firstName} {lastName}. I am {age} years old and work as a {occupation} in {location}."
-*/
+  @param { personObj } an array containing 
+  the names of a number of animals
+//   @returns { string } in the format...
+//     "Hi, my name is {firstName} {lastName}. 
+//     I am {age} years old and work as a {occupation} in {location}."
+// */
 
 // function question7( personObj ) {
 //   // TODO
 //   return /* ??? */;
 // }
 
+// console.log(question7( personObj ));
+
 
 /**
   +++++++++++++++++++++++++++++++++++++++++++
   Question 8:
-  Write a function to concatenate two arrays, then add an additional item to the resulting array. The two input arrays must not be modified.
+//   Write a function to concatenate two arrays, then add 
+//   an additional item to the resulting array. The two input
+//    arrays must not be modified.
 
-  @param { arr1, arr2, strExtra } two arrays, each containing 3 items, and a string value as the additional item
-  @returns { newArr } containing 7 items
-*/
+//   @param { arr1, arr2, strExtra } two arrays, each containing 
+//   3 items, and a string value as the additional item
+//   @returns { newArr } containing 7 items
+// */
+
+// const array1 = ["lions", "tigers", "bears"];
+// const array2 = ["penguins", "flamingoes", "eagles"];
+// const extra  = "cobras";
 
 // function question8( arr1, arr2, strExtra ) {
-//   // TODO
+//  console.log(array1.concat(array2, extra)); 
+//     // TODO
 //   return /* ??? */;
 // }
 
@@ -186,11 +222,11 @@ function question1(name1) {
 //     }
 //   }
 // }
-
+// 
 module.exports = {
   question1,
-  // question2,
-  // question3,
+  question2,
+  question3,
   // question4,
   // question5,
   // question6,
@@ -198,4 +234,4 @@ module.exports = {
   // question8, 
   // question9,
   // question10
-};
+  };
